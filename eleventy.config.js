da6543e209461f11b66a0d74bfcc7bdf8ee0bd3b
natebass <nw3455@gmail.om>
2023-08-10 03:54:26 -0700

    Configure Eleventy to use yml data files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,10 @@
+const yaml = require("js-yaml");
 /**
  * Configuration for eleventy
  * @parameter config
  */
 const eleventyConfig = config => {
+  config.addDataExtension("yml", contents => yaml.load(contents));
   config.ignores.add("README.md");
   config.ignores.add("blog/");
   config.ignores.add("_posts/");
